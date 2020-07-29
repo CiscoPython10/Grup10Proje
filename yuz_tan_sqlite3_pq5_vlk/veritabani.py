54c269bec88fe86000c06fc25d752582633c41c0
VolkanCINAR <68422817+VolkanCINAR@users.noreply.github.com>
2020-07-29 09:40:21 +0300

    29.07.2020_vlk_2

---
+++ yuz_tan_sqlite3_pq5_vlk/veritabani.py
@@ -0,0 +1,19 @@
+import sqlite3 # Sqlite'yı dahil ediyoruz
+
+con = sqlite3.connect("yuz_tan_vlk_deneme/yuz.db") # Tabloya bağlanıyoruz.
+
+cursor = con.cursor() # cursor isimli değişken veritabanı üzerinde işlem yapmak için kullanacağımız imleç olacak.
+
+def tablo_oluştur():
+    cursor.execute("CREATE TABLE IF NOT EXISTS yuz (İsim TEXT,  ID INT)") # Sorguyu çalıştırıyoruz.
+    con.commit() # Sorgunun veritabanı üzerinde geçerli olması için commit işlemi gerekli.
+def deger_ekle(isim,id):
+    cursor.execute("INSERT INTO yuz VALUES(?,?)",(isim,id))
+    con.commit()
+def verileri_al3(id):
+    cursor.execute("Select * From yuz where id = ?",(id,)) # Girilen idler göre işlem yapıyoruz
+    data = cursor.fetchall()
+    return data
+def veri_tabanikapat():
+    con.close()
+               
