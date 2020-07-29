3362f689c9f05950ca359e57909cbe730c35c7e5
VolkanCINAR <68422817+VolkanCINAR@users.noreply.github.com>
2020-07-29 10:33:17 +0300

    20072020_vlk_3

--- yuz_tan_sqlite3_pq5_vlk/anamenu.py
+++ yuz_tan_sqlite3_pq5_vlk/anamenu.py
@@ -1,4 +1,6 @@
 import sys,os
+from PyQt5.QtWidgets import QApplication,QWidget
+from PyQt5 import uic
 from PyQt5 import QtWidgets,QtGui
 import cv2
 from PyQt5.QtCore import QTimer
@@ -16,64 +18,34 @@ class Pencere(QtWidgets.QWidget):
         self.init_ui()# tüm işlemlerin yapılacağı kısım
 
     def init_ui(self):
-        self.yazi_alanı=QtWidgets.QLabel("İşlem Başlamadı")
-        self.buton=QtWidgets.QPushButton("Kamerayı Aç")
-        self.buton1=QtWidgets.QPushButton("Kamerayı Kapat")
-        self.buton2=QtWidgets.QPushButton("VeriSeti Oluştur")
-        self.kisi_adi=QtWidgets.QLineEdit()
-        self.kisi_adi_label=QtWidgets.QLabel("Adı:")
-        self.kisi_id=QtWidgets.QLineEdit()
-        self.kisi_id_label=QtWidgets.QLabel("ID Numarası")
-        self.buton3=QtWidgets.QPushButton("Eğit")
-        self.egit_bilgi=QtWidgets.QTextEdit("")
-        self.buton4=QtWidgets.QPushButton("Tanımla")
-        self.kamera_label=QtWidgets.QLabel("Kamera Label")
+       
+        self.win = uic.loadUi("yuz_tan_sqlite3_pq5_vlk/form.ui")
         self.say=0      
-        v_box=QtWidgets.QVBoxLayout()
-        v_box.addWidget(self.buton)
-        v_box.addWidget(self.buton1)
-        v_box.addWidget(self.buton2)
-        v_box.addWidget(self.kisi_adi_label)
-        v_box.addWidget(self.kisi_adi)
-        v_box.addWidget(self.kisi_id_label)
-        v_box.addWidget(self.kisi_id)
-        v_box.addWidget(self.buton3)
-        v_box.addWidget(self.egit_bilgi.setVisible(False))
-        v_box.addWidget(self.buton4)
-        v_box.addWidget(self.yazi_alanı)
-        v_box.addWidget(self.kamera_label)
-        v_box.addStretch()
-        h_box=QtWidgets.QHBoxLayout()
-        h_box.addStretch()
-        h_box.addLayout(v_box)
-        h_box.addStretch()
-        self.setLayout(h_box)
-        self.setWindowTitle("Yüz Tanımlama İşlemleri")
-        self.setGeometry(100,100,400,300)
         
-        self.buton.clicked.connect(self.click)
-        self.buton1.clicked.connect(self.kapat)
-        self.buton2.clicked.connect(self.veriseti)
-        self.buton3.clicked.connect(self.egitme)
-        self.buton4.clicked.connect(self.tanima)
-        self.show()
+        self.win.buton.clicked.connect(self.click)
+        self.win.buton1.clicked.connect(self.kapat)
+        self.win.buton2.clicked.connect(self.veriseti)
+        self.win.buton3.clicked.connect(self.egitme)
+        self.win.buton4.clicked.connect(self.tanima)
+        self.win.show()
 
 
     def tanima(self):
+       
         recognizer=cv2.face.LBPHFaceRecognizer_create()
-        recognizer.read('training/trainer.yml')
-        cascadePath = "face.xml"
+        recognizer.read('yuz_tan_sqlite3_pq5_vlk/training/trainer.yml')
+        cascadePath = "yuz_tan_sqlite3_pq5_vlk/face.xml"
         faceCascade = cv2.CascadeClassifier(cascadePath)
-        path = 'yuzverileri'
+        path = 'yuz_tan_sqlite3_pq5_vlk/yuzverileri'
         self.cam = cv2.VideoCapture(0)
         self.timer.start(3)
-        
-        self.yazi_alanı.setText("Tanımlama İşlemi Yapılıyor")
+        self.win.yazi_alani.setText("Tanımlama İşlemi Yapılıyor")
+
         while True:
             ret, im =self.cam.read()
-            
             gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
             faces=faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
+            #aces=face_cascade.detectMultiScale(gray,1.3,5)
             for(x,y,w,h) in faces:
                 tahminEdilenKisi, conf = recognizer.predict(gray[y:y + h, x:x + w])
                 cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
@@ -88,19 +60,19 @@ class Pencere(QtWidgets.QWidget):
                 fontColor = (255, 255, 255)
                 cv2.putText(im, str(tahminEdilenKisi), (x, y + h), fontFace, fontScale, fontColor)
                 
-                cv2.imshow('im',im)
+                #cv2.imshow('im',im)
 
-                # im = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-                # height,width,channel = im.shape
-                # step = channel*width
-                # qImg = QImage(im.data,width,height,step,QImage.Format_RGB888)
-                # self.win.kamera_label.setPixmap(QPixmap.fromImage(qImg))
+            im = cv2.cvtColor(im,cv2.COLOR_BGR2RGB)
+            height,width,channel = im.shape
+            step = channel*width
+            qImg = QImage(im.data,width,height,step,QImage.Format_RGB888)
+            self.win.kamera_label.setPixmap(QPixmap.fromImage(qImg))
 
-                if cv2.waitKey(25) & 0xFF == ord('q'):
-                    break 
+            if (cv2.waitKey(1) & 0xFF == ord("q")):
+                break
             
-            self.cam.release() 
-            timer1.stop()             
+        self.cam.release() 
+        self.timer.stop()             
 
     def kapat(self):
         self.yazi_alanı.setText("Kamera Kapatıldı")
@@ -128,9 +100,9 @@ class Pencere(QtWidgets.QWidget):
         self.yazi_alanı.setText("Eğitime İşlemleri Yapılıyor")
         if not self.timer.isActive():
             recognizer = cv2.face.LBPHFaceRecognizer_create()
-            cascadePath = "face.xml"
+            cascadePath = "yuz_tan_sqlite3_pq5_vlk/face.xml"
             faceCascade = cv2.CascadeClassifier(cascadePath)
-            path = 'yuzverileri'
+            path = 'yuz_tan_sqlite3_pq5_vlk/yuzverileri'
             def get_images_and_labels(path):
                 image_paths = [os.path.join(path, f) for f in os.listdir(path)]
                 images = []
@@ -154,7 +126,7 @@ class Pencere(QtWidgets.QWidget):
             cv2.waitKey(1)
 
             recognizer.train(images, np.array(labels))
-            recognizer.write('yuz_tan_vlk_deneme/training/trainer.yml')
+            recognizer.write('yuz_tan_sqlite3_pq5_vlk/training/trainer.yml')
             cv2.destroyAllWindows()
         else:
             self.timer.stop()
@@ -166,7 +138,7 @@ class Pencere(QtWidgets.QWidget):
         if not self.timer.isActive():
             
             self.kamera=cv2.VideoCapture(0)
-            self.detector=cv2.CascadeClassifier('yuz_tan_vlk_deneme/face.xml')
+            self.detector=cv2.CascadeClassifier('yuz_tan_sqlite3_pq5_vlk/face.xml')
             kisi_id=self.kisi_id.text()
             kisi_adi=self.kisi_adi.text()
             kisi_adi=kisi_adi.upper()
@@ -181,7 +153,7 @@ class Pencere(QtWidgets.QWidget):
                 faces=self.detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
                 for(x,y,w,h) in faces:
                     i=i+1
-                    cv2.imwrite("yuz_tan_vlk_deneme/yuzverileri/face-" + kisi_id + '.' + str(i) + ".jpg", gray[y:y + h , x :x + w])
+                    cv2.imwrite("yuz_tan_sqlite3_pq5_vlk/yuzverileri/face-" + kisi_id + '.' + str(i) + ".jpg", gray[y:y + h , x :x + w])
                     cv2.rectangle(img, (x , y), (x + w, y + h), (225, 0, 0), 2)
                     cv2.imshow('resim', img[y :y + h, x :x + w])
                     cv2.waitKey(100)
